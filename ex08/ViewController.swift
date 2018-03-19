11395d161e27c130830a68ea9fe081a65987a288
Hank Wu <hankwu61@gmail.com>
2018-03-19 23:33:55 +0800

    Initial Commit

---
+++ ex08/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ex08
+//
+//  Created by HSIN-HUNG WU on 2018/3/19.
+//  Copyright © 2018年 HSIN-HUNG WU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
